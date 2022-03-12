723b95be69233a1ec0ffde2f378f2c4ade555088
George <georgeXen89@gmail.com>
2022-03-12 18:11:08 +0200

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-//npm  i react-typical --force
-//npm i rxjs
-//npm i  @fortawesome/fontawesome-svg-core 
-//npm i @fortawesome/free-brands-svg-icons 
-//npm i @fortawesome/free-regular-svg-icons
-//npm i @fortawesome/free-solid-svg-icons
-//npm i @fortawesome/react-fontawesome
-//npm i react-owl-carousel
-//npm i axios react-toastify
-//npm i concurrently cors dotenv express nodemailer nodemon
-
 import "./App.css";
 import PortfolioContainer from "./PortfolioContainer/PortfolioContainer";
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter, Route } from 'react-router-dom';
+import PageBooks from "./PortfolioContainer/Interests/Booklover";
+import basketball from "./PortfolioContainer/Interests/basketball";
 
-
-
-function App() {
-  return <div className="App">
-    <ToastContainer />
-    <PortfolioContainer />
-  </div >;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Route path='/' exact>
+          <ToastContainer />
+          <PortfolioContainer />
+        </Route>
+        <Route path='/books' component={PageBooks} />
+        <Route path='/basketball' component={basketball} />
+      </BrowserRouter>
+    </div >
+  );
+};
 
 export default App;
+
+
